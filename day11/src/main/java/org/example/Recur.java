f90893efdbbd1dbd7690ae06cc528e0c77e11b17
kangkigs <dlflq11@gmail.com>
2024-06-10 11:18:14 +0900

    재귀함수 팩토리얼 예제 구현

--- day11/src/main/java/org/example/Recur.java
+++ day11/src/main/java/org/example/Recur.java
@@ -4,5 +4,11 @@ public class Recur {
     //재귀 함수
     //메소드가 자기 자신을 실행하는 형태
 
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+
+        System.out.println(f.calc(5));
+    }
+
 
 }
