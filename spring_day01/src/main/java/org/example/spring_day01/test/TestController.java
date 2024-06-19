54f320b989dc940136a4fceb907be8a087cf92a1
kangkigs <dlflq11@gmail.com>
2024-06-19 10:14:39 +0900

    스프링 부트 기본 예제코드 작성

--- spring_day01/src/main/java/org/example/spring_day01/test/TestController.java
+++ spring_day01/src/main/java/org/example/spring_day01/test/TestController.java
@@ -1,8 +1,15 @@
 package org.example.spring_day01.test;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/test")
 public class TestController {
-
+    @RequestMapping(method = RequestMethod.GET, value = "/ex01")
+    public String ex01(){
+        return "abcd";
+    }
 }
