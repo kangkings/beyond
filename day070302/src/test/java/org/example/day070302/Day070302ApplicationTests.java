903633a195d0a118620859f110e3eb1b5d957a36
kangkigs <dlflq11@gmail.com>
2024-07-03 16:36:36 +0900

    kafka 통신 테스트

---
+++ day070302/src/test/java/org/example/day070302/Day070302ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day070302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day070302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
