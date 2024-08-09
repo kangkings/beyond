cb24e16d0f4a34e91a7a52fa1120c567bb62130c
kangkigs <dlflq11@gmail.com>
2024-08-09 15:28:22 +0900

    테스트2

--- devopstest/src/main/java/org/example/devopstest/Controller.java
+++ devopstest/src/main/java/org/example/devopstest/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
     @GetMapping("/version")
     public String version(){
         System.out.println("푸시테스트~~~");
+        System.out.println("푸시테스트~git ~~");
         return "v1";
     }
 }
